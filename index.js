067b2732119205cb46cc7b159592c3d7f3f8a954
Alex Blom <alexblom8@gmail.com>
2025-02-05 01:58:29 +0100

    backend url update

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ require('dotenv').config()
 //middleware
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://e-bookstore-app.vercel.app'],
+    origin: ['http://localhost:5173', 'https://e-bookstore-app.vercel.app', 'https://e-bookstore-app-backend.vercel.app'],
     credentials: true
 }));
 
